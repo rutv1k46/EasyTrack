9c7e9a0250c51feb7b3d93dd418cca0d52a315ec
rutv1k46 <rutviksolanki01@gmail.com>
2024-06-25 15:25:01 -0400

    input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
+  selectedUserId = "u1";
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed, Input, input } from '@angular/core';
+import { Component, signal, computed, Input, input, Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -12,22 +12,25 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
 
-  // @Input({required: true}) avatar!:string
-  // @Input({required: true}) name!:string
+  @Input({required: true}) avatar!:string
+  @Input({required: true}) name!:string
+  @Input({required: false}) id!:string
+  @Output() select = new EventEmitter()
 
-  avatar = input.required<string>()
-  name = input.required<string>()
+  // avatar = input.required<string>()
+  // name = input.required<string>()
 
   // selectedUser = signal(DUMMY_USERS[randomIndex])
-  userImage = computed(() => {
-    'assets/users/' + this.avatar()
-  })
+  get userImage() {
+    return 'assets/users/' + this.avatar
+  }
 
   // get userImage() {
   //   return 'assets/users/' + this.avatar
   // }
 
   onSelectUser() {
+    this.select.emit(this.id)
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
     // this.selectedUser.set(DUMMY_USERS[randomIndex])
   }
